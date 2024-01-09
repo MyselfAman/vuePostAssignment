c27adb0a1af548a3ef79955a8b33ae59b7263143
MyselfAman <myselfamn77@gmail.com>
2024-01-09 14:09:16 +0530

    added small cahnges in routes so that user can easily navigate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 </script>
 
 <template>
   <main>
-    <h1>jhvbuhdscds</h1>
+    <div
+      class="d-flex align-center justify-center bg-indigo"
+      style="height: calc(100vh)"
+    >
+      <div class="d-flex flex-column">
+        <h1>Hi There , This is home page click on below button to see posts</h1>
+        <div class="d-flex justify-center">
+          <v-btn class="mt-6" @click="router.push('/posts')">View Posts</v-btn>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
